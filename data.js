46dde30061ab28838b3577728f183485f141f4c8
Lara Cook <190218@virtualwindow.co.za>
2020-10-08 20:08:50 +0200

    Fix footer responsiveness

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var shoesArray = [
     { id: "2", name: "Adidas Continental", price: "R1300", image: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" },
     { id: "3", name: "Nike Running Shoe", price: "R2500", image: "https://images.unsplash.com/photo-1518656306295-aa28b28b2504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" },
     { id: "4", name: "Nike Jordan 1 Knit", price: "R2000", image: "https://images.unsplash.com/photo-1578116922645-3976907a7671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80" },
-    { id: "5", name: "Nike Air Force 1", price: "R2500", image: "" },
-    { id: "6", name: "Nike Air Force 1", price: "R2500", image: "" },
-    { id: "7", name: "Nike Air Force 1", price: "R2500", image: "" },
+    { id: "5", name: "FILA Dinosaur Stompers", price: "R1400", image: "https://images.unsplash.com/photo-1579446565308-427218a2c60e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80" },
+    { id: "6", name: "Nike Jordan 13", price: "R2500", image: "https://images.unsplash.com/photo-1580143881495-b21dde95fc60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" },
+    { id: "7", name: "Nike Air Max Orange", price: "R999", image: "https://images.unsplash.com/photo-1600185365778-7875a359b924?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80" },
 ];
